61a5811d99f712023def9df76061e02e6963265b
wrb <rbao123@qq.com>
2020-07-23 15:50:07 +0800

    验证应用

--- src/parser/Examine.php
+++ src/parser/Examine.php
@@ -11,13 +11,20 @@ class Examine
 {
     public $blacklist;
     public $verification;
+    public $combination;
 
-    public function __construct(Blacklist $blacklist)
+    public function __construct(Blacklist $blacklist, Combination $combination)
     {
         $this->blacklist = $blacklist;
+        $this->combination = $combination;
     }
 
     public function setVerification($verification)
+    {
+        return $this->verification = $verification;
+    }
+
+    public function getVerification($verification)
     {
         return $this->verification = $verification;
     }
@@ -52,6 +59,11 @@ class Examine
      */
     public function checkApp()
     {
+        if ($this->combination->multiApp()) {
+            if ($this->verification = !$this->combination->getAppName()) {
+                throw new JWTException('该令牌不是本应用的');
+            }
+        }
         $this->checkTime();
     }
 
